b24afe32a6a34ca350d24cd3138903f86b322a37
root <root@host01.example.com>
2025-04-11 14:30:52 +0530

    sixth commit

--- create-topics.sh
+++ create-topics.sh
@@ -8,7 +8,7 @@ command -v yq >/dev/null || { echo "yq is required to parse YAML"; exit 1; }
 # Loop through topics
 yq e '.topics[]' "$TOPIC_FILE" | yq -e '.topics[] | [.name, .partitions, .replicationFactor] | @tsv' "$TOPIC_FILE" | while IFS=$'\t' read -r NAME PARTITIONS REPLICAS; do
   echo "Creating topic: $NAME"
-  kafka-topics --create --bootstrap-server localhost:9092 \
+  /etc/confluent-7.8.0/bin/kafka-topics --create --bootstrap-server localhost:9092 \
     --replication-factor "$REPLICAS" \
     --partitions "$PARTITIONS" \
     --topic "$NAME"
